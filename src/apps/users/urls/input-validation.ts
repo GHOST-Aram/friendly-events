774581544e43d9874a653e149434c607a03faea2
GHOST-Aram <owinoojala@gmail.com>
2024-07-21 14:10:39 +0300

    refactor: Update user input validators
              - Add validators for the new user data properties
              - Remove validators for user properties that have been dropped

--- src/apps/users/urls/input-validation.ts
+++ src/apps/users/urls/input-validation.ts
@@ -2,26 +2,25 @@ import { ValidationChain } from "express-validator";
 import { Validator } from "../../../z-library/validation/validator";
 
 class UserValidator extends Validator{
-    public validateRole  = ( { required }: { required: boolean }): ValidationChain =>{
-        return this.validateString('role', { required })
+    public validateUserGroup  = ( { required }: { required: boolean }): ValidationChain =>{
+        return this.validateString('userGroup', { required })
             .custom((value: string) =>{
-            return value === 'tenant' || value === 'landlord'
-        }).withMessage('role must be either \'tenant\' or \'landlord\'')
+            return value === 'attendee' || value === 'host' || value === 'superuser'
+        }).withMessage('User Group can only be  \'host\' or \'attendee\' or \'superuser\'')
     }
 }
 
 const validator = new UserValidator()
 
 export const userValidators = [
-    validator.validateRole({ required: true }),
+    validator.validateUserGroup({ required: true }),
     validator.validateName('fullName', { required: true}),
     validator.validateString('password', { required: true}),
     validator.validateString('email', { required: true}),
-    validator.validateBooleanField('isAdmin', { required: false})
 ]
 
 export const patchValidators = [
-    validator.validateRole({ required:false }),
+    validator.validateUserGroup({ required:false }),
     validator.validateName('fullName', { required: false}),
     validator.validateString('email', { required: false}),
     validator.validateString('password', { required: false})
--- src/apps/users/utils/validator.ts
+++
@@ -1,5 +0,0 @@
-import { Validator } from "../../../z-library/validation/validator";
-
-class UserValidator extends Validator{}
-
-export const validator = new UserValidator()
