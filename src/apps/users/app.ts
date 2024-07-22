39657e0aa5ff991e5183da21b18a4d04e31d3cba
GHOST-Aram <owinoojala@gmail.com>
2024-07-22 12:55:35 +0300

    fix: Handle Database connection switching errors in Users app
         - Catch the error that may be thrown if the connection object is undefined
           due to unsuccessfull database connection / missing databse string

--- src/apps/users/app.ts
+++ src/apps/users/app.ts
@@ -5,12 +5,19 @@ import { connection } from "../../config/config";
 import { DB } from "../../z-library/db/db";
 import { userSchema } from "./data-access/model";
 import { authenticator } from "../../z-library/auth/auth";
+import { Router } from "express";
 
+let usersRouter: Router
+try {
+    const db = new DB(connection.switch('homenest-users'))
+    const UserModel = db.createModel('User', userSchema)
+    
+    const usersDAL = new UsersDAL(UserModel)
+    const controller = new UsersController(usersDAL, 'users')
+    usersRouter = routesWrapper(controller, authenticator)
+    
+} catch (error: any) {
+    console.log("Error occured while Switching to Users Database: \n", error.message)
+}
 
-const db = new DB(connection.switch('homenest-users'))
-const UserModel = db.createModel('User', userSchema)
-
-const usersDAL = new UsersDAL(UserModel)
-const controller = new UsersController(usersDAL, 'users')
-
-export const usersRouter = routesWrapper(controller, authenticator)
+export { usersRouter }
