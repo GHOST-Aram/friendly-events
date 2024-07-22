5b39aa7ffe0db2c3952e389d2c53cc525eaddd2e
GHOST-Aram <owinoojala@gmail.com>
2024-07-22 22:25:54 +0300

    refactor: Merge Files with related code
             - Merge valation files in authenticator
             - Delete authenticator.utils folder

--- src/apps/authenticator/routes/input-validators.ts
+++ src/apps/authenticator/routes/input-validators.ts
@@ -1,4 +1,20 @@
-import { validator } from "../utils/validator";
+import { Validator } from "../../../z-library/validation/validator";
+import { body } from "express-validator";
+
+class AuthValidator extends Validator{
+    public validateEmail = (field: string) =>{
+        return body(field).trim().notEmpty().withMessage('Email is required')
+            .escape()
+    }
+
+    public validatePassword = (field: string) =>{
+        return body(field).notEmpty().withMessage('Password is required')
+            .isLength({ min: 8, max: 100 }).withMessage(
+                'Password must be a string of 8 - 100 chars long')
+    }
+}
+
+export const validator = new AuthValidator()
 
 export const validateLoginInput = [
     validator.validateEmail('email'),
