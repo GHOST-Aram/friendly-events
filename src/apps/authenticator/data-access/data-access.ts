1e68030b22efcfc7851c6a837210e7a408f26465
GHOST-Aram <owinoojala@gmail.com>
2024-07-22 12:53:34 +0300

    fix: Handle database connection switching errors in Authenticator
         - Move the database switching code into the Database access class method
         - Switch DB during database operation
         - This will forward the error handling to the caller of the data access method

--- src/apps/authenticator/data-access/data-access.ts
+++ src/apps/authenticator/data-access/data-access.ts
@@ -1,13 +1,14 @@
 import  { HydratedDocument } from "mongoose"
 import { connection } from "../../../config/config"
 
-const authDbConnection = connection.getInitial()
+
 
 
 export class DataAccess{
   
-    public findUserByEmail = async(email: string
-        ): Promise<HydratedDocument<any> | null> =>{
-       return await authDbConnection.db.collection('users').findOne({ email })
+    public findUserByEmail = async(email: string): Promise<HydratedDocument<any> | null> =>{
+            const authDbConnection = connection.getInitial()
+            
+            return await authDbConnection.db.collection('users').findOne({ email })
     }    
 }
