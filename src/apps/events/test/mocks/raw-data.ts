ffb4c0364183d94ab2768bb2f6cc6b2c6eee0de0
GHOST-Aram <owinoojala@gmail.com>
2024-07-23 13:38:51 +0300

    refactor: Provide raw data for testing events POST route

--- src/apps/events/test/mocks/raw-data.ts
+++ src/apps/events/test/mocks/raw-data.ts
@@ -1,11 +1,39 @@
 import { Event } from '../../data-access/model'
 
-export const validEventData= {
-   
-}
-
-export const upcomingEvent = {//Already created - to test conflicts
+export const validEventData: Event= {
+    category: "Music Concert",
+    venue: "Quiver Lounge",
+    title:"Ramogi Night",
+    city: "Nairobi",
+    date: "23 July 2024",
+    time: {
+        start: "12:37:48 PM",
+        end: "12:37:48 PM",
+        zone: "GMT+03:00"
+    },
+    duration: " 3 days",
+    ageLimit: {
+        min: 17,
+        max: 25
+    },
+    availableTickets: 100,
+    ticketPrice: 2500
 }
 
 export const invalidEventData = {
+    category: "",
+    title:"",
+    city: "",
+    date: "7/23/2024",
+    time: {
+        start: "2200hrs",
+        end: "0000hrs",
+    },
+    duration: "3",
+    ageLimit: {
+        min: "17red",
+        max: "25trf"
+    },
+    availableTickets: "One hundred",
+    ticketPrice: "One hindred"
 }
