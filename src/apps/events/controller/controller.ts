eaa909372a0ffa238930b21a756d09c1c06b7ec9
GHOST-Aram <owinoojala@gmail.com>
2024-07-23 10:52:52 +0300

    feat: Create events controller class

--- src/apps/events/controller/controller.ts
+++ src/apps/events/controller/controller.ts
@@ -1,3 +1,10 @@
 import { Response, Request, NextFunction } from "express";
 import { GenericController } from "../../../z-library/bases/generic-controller";
+import { EventsDataAccess } from "../data-access/data-access";
+
+export class EventsController extends GenericController<EventsDataAccess>{
+    constructor (dataAccess: EventsDataAccess, microserviceName: string){
+        super(dataAccess, microserviceName)
+    }
+}
 
