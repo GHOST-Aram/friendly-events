e8cce693d5dcb5f5f138cce0be4819558d8edad9
GHOST-Aram <owinoojala@gmail.com>
2024-07-23 11:13:11 +0300

    refactor: Configure Express for events app tests

--- src/apps/events/test/config/app.ts
+++ src/apps/events/test/config/app.ts
@@ -1,11 +1,18 @@
+import { EventsController } from "../../controller/controller";
 import { routesWrapper } from "../../urls/urls";
 import express from "express"
+import { EventsDAL } from "../mocks/data-access";
+import { Event } from "../../data-access/model";
+import { authenticator } from "../mocks/auth";
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+const dataAccess = new EventsDAL(Event)
+const controller = new EventsController(dataAccess, 'events')
 
+app.use('/events', routesWrapper(controller, authenticator))
 
 export { app }
