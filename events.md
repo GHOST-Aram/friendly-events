365aaf283b008380a3b7d0227dc9809065329ffb
GHOST-Aram <owinoojala@gmail.com>
2024-07-23 04:58:59 +0300

    feat: Create Event model
          - Create a data model for the event app

--- events.md
+++ events.md
@@ -1,37 +1,13 @@
 
-## Event data points
-- Event category,
-- Event title
-- Graphic
-- City
-
-General Info
-- venue 
-- Date and time
-- Duration
-- Age restriction
-- Venue Accessibility
-- Event Description
-- Event ticket price
-- Available tickets
-
 ## Categories
 - Category name
 - Category Graphic
+- Category description
 
 ## Venues
+- Venue Accessibility
 - Name
 - Location
 - LatLong
 - Venue Images
 - Venue Description
-
-## User
-- fullName
-- email
-- password
-- designation: {
-    eventOrganizer,
-    attendee,
-    admin
-}
--- src/apps/events/data-access/model.ts
+++ src/apps/events/data-access/model.ts
@@ -1,2 +1,64 @@
-import { HydratedDocument, Model, Schema, model } from "mongoose"
+import { HydratedDocument, Model, Schema, model, ObjectId } from "mongoose"
+
+export interface Event{
+    category: ObjectId
+    venue: ObjectId
+    title: string
+    graphic?: Buffer
+    city: string
+    dateAndTime: string
+    duration: string
+    ageLimit: {
+        min: number,
+        max: number
+    }
+    availableTickets: number
+    ticketPrice: number
+}
+
+export type EventModel = Model<Event>
+
+const eventSchema = new Schema<Event, EventModel>({
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'EventCategory',
+        required: true
+    },
+    venue: {
+        type: Schema.Types.ObjectId,
+        ref: 'Venue',
+        required: true
+    },
+
+    title: String,
+    graphic: {
+        name: String,
+        data: Buffer,
+        contentType: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    dateAndTime: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    ageLimit: {
+        min: {
+            type: Number,
+            required: true
+        },
+        max: Number,
+    },
+    availableTickets: {
+        type: Number,
+        required: true
+    }
+
+})
 
