dd7f849377295c0513694a751a31dc457e198b93
GHOST-Aram <owinoojala@gmail.com>
2024-07-23 13:52:30 +0300

    refactor: Decouple validation path names from EventValidator methods
             - Inject path names to validation methods as parameters

--- src/apps/events/urls/input-validation.ts
+++ src/apps/events/urls/input-validation.ts
@@ -1,8 +1,8 @@
 import { Validator } from "../../../z-library/validation/validator";
 
 class EventsValidator extends Validator{
-    public validateDate = ({ required }: {required: boolean }) =>{
-        return this.validateString('date', { required })
+    public validateDate = (path: string, { required }: {required: boolean }) =>{
+        return this.validateString(path, { required })
             .matches(/^[0-9]{1,2}\s[a-z]+\s[0-9]{4}$/i)
             .withMessage(
                 "Date String must be in the form \'day MonthName year\' eg \'4 January 2027\'")
@@ -21,8 +21,8 @@ class EventsValidator extends Validator{
                 'eg GMT+03:00')
     }
 
-    public validateDuration = ({ required }: { required: boolean }) =>{
-        return this.validateString('duration', { required })
+    public validateDuration = (path: string, { required }: { required: boolean }) =>{
+        return this.validateString(path, { required })
             .matches(/[0-9]{2}\s[days|hours|minutes]/i).withMessage(
                 'Duration must be in the for \'X days or X hours or X minutes\'')
     }
@@ -35,11 +35,11 @@ export const postValidators = [
     validator.validateString('venue', { required: true }),
     validator.validateString('title', { required: true }),
     validator.validateString('city', { required: true }),
-    validator.validateDate( { required: true }),
+    validator.validateDate('date', { required: true }),
     validator.validateTime('time.start', {required: true }),
     validator.validateTime('time.end', {required: true }),
     validator.validateTimeZone('time.zone', {required: true }),
-    validator.validateDuration({ required: true }),
+    validator.validateDuration('duration', { required: true }),
     validator.validateNumber('availableTickets', { required: true }),
     validator.validateNumber('ticketPrice', { required: true }),
     validator.validateNumber('ageLimit.min', { required: true }),
@@ -50,11 +50,11 @@ export const patchValidators = [
     validator.validateString('venue', { required: false }),
     validator.validateString('title', { required: false }),
     validator.validateString('city', { required: false }),
-    validator.validateDate( { required: false }),
+    validator.validateDate('date', { required: false }),
     validator.validateTime('time.start', {required: false }),
     validator.validateTime('time.end', {required: false }),
     validator.validateTimeZone('time.zone', { required: false }),
-    validator.validateDuration({ required: false }),
+    validator.validateDuration('date',{ required: false }),
     validator.validateNumber('availableTickets', { required: false }),
     validator.validateNumber('ticketPrice', { required: false }),
     validator.validateNumber('ageLimit.min', { required: false }),
