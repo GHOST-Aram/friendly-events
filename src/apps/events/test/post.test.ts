8acc169f1c179b4382307027b711b970aed48dfd
GHOST-Aram <owinoojala@gmail.com>
2024-07-23 11:35:38 +0300

    chore: Write tests for Events POST route

--- src/apps/events/test/post.test.ts
+++ src/apps/events/test/post.test.ts
@@ -2,5 +2,32 @@ import { assert} from "../../../z-library/testing/response-assertion";
 import { describe, test } from '@jest/globals'
 import request from 'supertest'
 import {app} from './config/app'
+import * as data from './mocks/raw-data'
 
 
+describe('POST events', () =>{
+
+    test('Rejects request with client defined Id (status 405): Method not allowed. ', 
+        async() =>{
+            const response = await request(app).post('/events/64c9e4f2df7cc072af2ac9e4')
+            assert.respondsWithMethodNotAllowed(response)
+        }
+    )
+
+    test('Responds with validation errors (status 400): Invalid input.', 
+        async() =>{
+            const response = await request(app).post('/events').send(data.invalidEventData)
+
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+
+        }
+    )
+
+    test('Responds with created resource (status 201): Operation Success.', 
+        async() =>{
+            const response = await request(app).post('/events').send(data.validEventData)
+            assert.respondsWithCreatedResource(response)
+        }
+    )  
+})
