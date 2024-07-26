0e3b70a773efc07f4a33884201b57ded5d093006
GHOST-Aram <owinoojala@gmail.com>
2024-07-26 13:46:09 +0300

    feat: Reject unwanted input data properties in categories app

--- src/apps/categories/urls/input-validation.ts
+++ src/apps/categories/urls/input-validation.ts
@@ -10,12 +10,15 @@ class VenuesValidator extends Validator{
 }
 
 const validator = new VenuesValidator()
+const acceptedPaths = ['name', 'description']
 
 const validatePostData: ValidationChain[] = [
+    validator.rejectUnwantedPaths(acceptedPaths),
     validator.validateName('name', { required: true }),
     validator.validateDescription('description', { required:true })
 ]
 const validatePatchData: ValidationChain[] = [
+    validator.rejectUnwantedPaths(acceptedPaths),
     validator.validateName('name', { required: false }),
     validator.validateDescription('description', { required:false })
 ]
