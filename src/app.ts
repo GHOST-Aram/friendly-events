195eed906a13886aa9195b01f1a93225d4586cc4
GHOST-Aram <owinoojala@gmail.com>
2024-07-26 14:40:37 +0300

    chore: Rename categories app to event-categories

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { authRouter } from "./apps/authenticator/app";
 import { httpErrors } from "./z-library/HTTP/http-errors";
 import { eventsRouter } from "./apps/eventz/app";
 import { venuesRouter } from "./apps/venues/app";
-import { categoriesRouter } from "./apps/categories/app";
+import { categoriesRouter } from "./apps/event-categories/app";
 
 
 try {
@@ -12,7 +12,7 @@ try {
     app.use('/users', usersRouter)
     app.use('/events', eventsRouter)
     app.use('/venues', venuesRouter)
-    app.use('/categories', categoriesRouter)
+    app.use('/event-categories', categoriesRouter)  
     
 } catch (error:any) {
     console.warn("Error occured while configuring routes: ", error.message)
