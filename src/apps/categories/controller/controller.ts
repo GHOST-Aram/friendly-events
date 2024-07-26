f6f2ed07d183a026a211c866e8e5d0a67d08b661
GHOST-Aram <owinoojala@gmail.com>
2024-07-26 11:03:13 +0300

    feat: Include uploaded file in category update data
          - Override the GenericController.updateOne and modifyOne  method with
            an implementation that includes file data in the update document

--- src/apps/categories/controller/controller.ts
+++ src/apps/categories/controller/controller.ts
@@ -49,5 +49,29 @@ export class Controller extends GenericController<DataAccess>{
             next(error)
         }
     }
+
+    public modifyOne = async(req: Request, res: Response, next: NextFunction) =>{
+        const referenceId = req.params.id
+        const reqBody = req.body
+        const user:any = req.user
+        const file = req.file as Express.Multer.File
+
+        const inputData: EventCategory = { ...reqBody, createdBy: user._id }
+        const updateDoc = file ? domainData.includeFile(inputData, file) : inputData
+
+        try {
+            const updatedDoc = await this.dataAccess.findByIdAndUpdate(referenceId, 
+                updateDoc)
+
+            if(updatedDoc){
+                this.respondWithUpdatedResource(updatedDoc, res)
+            } else{
+                this.respondWithNotFound(res)
+            }
+
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
