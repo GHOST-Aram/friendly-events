fba93a5dabbcffd7fef71b95eaf6f03a58dc31c2
GHOST-Aram <owinoojala@gmail.com>
2024-07-26 10:22:34 +0300

    chore: Add Categories route to the systems's entry file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { authRouter } from "./apps/authenticator/app";
 import { httpErrors } from "./z-library/HTTP/http-errors";
 import { eventsRouter } from "./apps/eventz/app";
 import { venuesRouter } from "./apps/venues/app";
+import { categoriesRouter } from "./apps/categories/app";
 
 
 try {
@@ -11,6 +12,7 @@ try {
     app.use('/users', usersRouter)
     app.use('/events', eventsRouter)
     app.use('/venues', venuesRouter)
+    app.use('/categories', categoriesRouter)
     
 } catch (error:any) {
     console.warn("Error occured while configuring routes: ", error.message)
