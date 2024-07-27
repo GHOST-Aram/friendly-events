e5275a71c106934f56e6322981c46140971db73e
GHOST-Aram <owinoojala@gmail.com>
2024-07-27 20:08:29 +0300

    refactor: Include currentUserId property in the RequestData Object
             - Include currentUserId in the value returned by the function
               getDataFromRequest

--- src/z-library/request/request-data.ts
+++ src/z-library/request/request-data.ts
@@ -6,7 +6,7 @@ export const getDataFromRequest = (req: Request): RequestData =>{
     const user:any = req.user
     const file = req.file as Express.Multer.File
     const files = req.files as Express.Multer.File[]
-    const currentUserId: string = user ? user._id : ''
+    const currentUserId: string = user ? user._id.toString() : ''
 
     return { referenceId, reqBody, user, file, files, currentUserId }
 }
