5883779434b795f0a71e1ebc64b8b5692985a348
GHOST-Aram <owinoojala@gmail.com>
2024-07-28 10:51:38 +0300

    refactor:Implement the DomainData interface in venuetypes app

--- src/apps/venuetypes/domain/data.ts
+++ src/apps/venuetypes/domain/data.ts
@@ -1,3 +1,12 @@
-class DomainData{}
+import { DomainData } from "../../../z-library/bases/domain-data";
+import { RequestData } from "../../../z-library/request/request-data";
+import { VenueCategory } from "../data-access/model";
 
-export const domainData = new DomainData()
+class VenueTData implements DomainData{
+    public createInputDocument = (reqData: RequestData): VenueCategory =>{
+        const { user, reqBody } = reqData
+        return { ...reqBody, createdBy: user._id }
+    };
+}
+
+export const domainData = new VenueTData()
