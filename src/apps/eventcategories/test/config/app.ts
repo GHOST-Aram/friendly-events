e813eb86389234f12d3f83e0770e0093baa37307
GHOST-Aram <owinoojala@gmail.com>
2024-07-30 00:13:31 +0300

    refactor: Extend GhostRouter class in Event Categories and test

--- src/apps/eventcategories/test/config/app.ts
+++ src/apps/eventcategories/test/config/app.ts
@@ -1,11 +1,11 @@
 import { Controller } from "../../controller/controller";
-import { authenticateAndControlRoutes } from "../../urls/urls";
 import express from "express"
 import { CategoriesDAL } from "../mocks/data-access";
 import { EventCategory } from "../../data-access/model";
 import { Authenticator } from "../../../../z-library/testing/mocks/auth";
 import { user } from "../../../../z-library/testing/mocks/mock-user";
 import { validData } from "../mocks/raw-data";
+import { CategoryRouter } from "../../urls/urls";
 
 const app = express()
 
@@ -18,7 +18,7 @@ const controller = new Controller(dataAccess, 'categories')
 user.userGroup = 'superuser'
 const authenticator = new Authenticator(user)
 
-
-app.use('/categories', authenticateAndControlRoutes(controller, authenticator))
+const categoriesRouter = new CategoryRouter(controller, authenticator)
+app.use('/categories', categoriesRouter.authenticateAndControlRoutes())
 
 export { app }
