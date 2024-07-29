cd7394a24087ce4f7d25e52b92f08e7e0a658eb7
GHOST-Aram <owinoojala@gmail.com>
2024-07-29 23:43:48 +0300

    refactor: Extend the GhostRouter class in the users urls file
            - Create a class called UsersRouter that extends the GhostRouter
              class and overrides the class method
            - Test the new UsersRouter class in the users tests to see that it works
              as expected

--- src/apps/users/test/config/app.ts
+++ src/apps/users/test/config/app.ts
@@ -1,4 +1,3 @@
-import { authenticateAndControlRoutes } from "../../urls/urls";
 import { UsersDAL } from "../mocks/data-access";
 import { UsersController } from "../../controller/controller";
 import express from "express"
@@ -6,6 +5,7 @@ import { User } from "../../data-access/model";
 import { Authenticator } from "../mocks/auth";
 import { user } from "../../../../z-library/testing/mocks/mock-user";
 import { validUserData } from "../mocks/raw-data";
+import { UsersRouter } from "../../urls/urls";
 
 const app = express()
 
@@ -16,6 +16,8 @@ const usersDAL = new UsersDAL(User, validUserData)
 const controller = new UsersController(usersDAL, 'users')
 
 const authenticator = new Authenticator(user)
-app.use('/users', authenticateAndControlRoutes(controller, authenticator))
+
+const usersRouter = new UsersRouter(controller, authenticator)
+app.use('/users', usersRouter.authenticateAndControlRoutes())
 
 export { app }
--- src/apps/users/urls/urls.ts
+++ src/apps/users/urls/urls.ts
@@ -1,10 +1,69 @@
 import { UsersController } from "../controller/controller";
-import { Router } from "express";
 import { validator, validationChains} from "./input-validation";
 import { Authenticator } from "../../../z-library/auth/auth";
 import { fileUploader } from "../../../z-library/uploads/upload";
 import { permission } from "../../../utils/permissions";
 import { domainData } from "../domain/data";
+import { GhostRouter } from "../../../z-library/routing/router";
+import { Router } from "express";
+
+
+export class UsersRouter extends GhostRouter{
+    constructor(controller: UsersController, authenticator: Authenticator){
+        super(controller, authenticator)
+    }
+
+    public authenticateAndControlRoutes = () =>{
+    
+        this.router.post('/:id', this.controller.respondWithMethodNotAllowed)
+        this.router.post('/', 
+            fileUploader.uploadSingleFile('profilePicture'),
+            validator.validateFile,
+            validationChains.validatePostData ,
+            validator.handleValidationErrors,
+            this.controller.addNew
+        )
+        
+        this.router.get('/', 
+            this.authenticator.authenticate(),
+            this.authenticator.restrictAccess(permission.allowAdmin),
+            this.controller.getMany
+        )
+        
+        this.router.get('/:id',
+            this.authenticator.authenticate(), 
+            validator.handleValidationErrors,
+            this.controller.getOne
+        )
+        
+        this.router.put('/', this.controller.respondWithMethodNotAllowed)
+        this.router.put('/:id', 
+            this.authenticator.authenticate(),
+            domainData.allowDocumentOwner,
+            fileUploader.uploadSingleFile('profilePicture'),
+            validator.validateFile,
+            validationChains.validatePostData, 
+            validator.handleValidationErrors,
+            this.controller.updateOne
+        )
+        
+        this.router.patch('/', this.controller.respondWithMethodNotAllowed)
+        this.router.patch('/:id', 
+            this.authenticator.authenticate(),
+            domainData.allowDocumentOwner,
+            fileUploader.uploadSingleFile('profilePicture'),
+            validator.validateFile,
+            validationChains.validatePatchData, 
+            validator.handleValidationErrors,
+            this.controller.modifyOne
+        )
+    
+        this.router.delete('/', this.controller.respondWithMethodNotAllowed)
+        this.router.delete('/:id', this.controller.respondWithMethodNotAllowed)
+    
+        return this.router
+    }
+}
 
 const router = Router()
 
