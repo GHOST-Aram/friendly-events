8a010430c0e8282a24c9fae3d8de8d74f66db9a3
GHOST-Aram <owinoojala@gmail.com>
2024-07-31 16:28:49 +0300

    Revert "refactor: Change database connection method for Authenticator"
    This reverts commit fa5e695dcfd716ce68bd2414aa60e43ee76b8483.

--- src/apps/authenticator/data-access/data-access.ts
+++ src/apps/authenticator/data-access/data-access.ts
@@ -7,8 +7,8 @@ import { connectionPool } from "../../../_config/config"
 export class DataAccess{
   
     public findUserByEmail = async(email: string): Promise<HydratedDocument<any> | null> =>{
-        const authDbConnection = connectionPool.switchConnection('users')
-        
-        return await authDbConnection.db.collection('users').findOne({ email })
+            const authDbConnection = connectionPool.getInitialConnection()
+            
+            return await authDbConnection.db.collection('users').findOne({ email })
     }    
 }
