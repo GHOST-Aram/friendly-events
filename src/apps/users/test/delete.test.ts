aec0a2bb6ca82987e02d1baea5c0cc2b61458b56
GHOST-Aram <owinoojala@gmail.com>
2024-08-01 13:30:02 +0300

    test: Add tests for reference Id validation in users app
         - Check if the user id provided in url parameters of
           GET, PATCH, PUT and DELETE requests is a valid
           mongodb id

--- src/apps/users/test/delete.test.ts
+++ src/apps/users/test/delete.test.ts
@@ -11,6 +11,15 @@ describe('DELETE users route', () =>{
         }
     )
 
+    test('Responds with Validation Errors (status 400): Invalid reference Id', 
+        async() =>{
+            const response = await request(app).delete('/users/64c9e4f2df7cc072af2acxx')
+                
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+        }
+    )
+
     test('Responds with Not Found (status 404): User does not exist.', 
         async() =>{
             const response = await request(app).delete(
--- src/apps/users/test/put.test.ts
+++ src/apps/users/test/put.test.ts
@@ -15,10 +15,19 @@ describe('PUT users route', () =>{
         }
     )
 
+    test('Responds with Validation Errors (status 400): Invalid reference Id', 
+        async() =>{
+            const response = await request(app).put('/users/64c9e4f2df7cc072af2acxx')
+                .send(rawData.validUserData)
+                
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+        }
+    )
+
     test('Responds with validation errors (status 400): Invalid input.', 
         async() =>{
-            const response = await request(app).put(
-                '/users/64c9e4f2df7cc072af2ac9e4')
+            const response = await request(app).put('/users/64c9e4f2df7cc072af2ac9e4')
                 .send(rawData.invalidUserData)
 
             assert.respondsWithBadRequest(response)
