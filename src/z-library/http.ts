bbcc28aa5380e6ad838cfe825a30cb8e7d2b6d7b
GHOST-Aram <owinoojala@gmail.com>
2024-08-02 23:28:40 +0300

    refactor: Export paginator interface from http import-export file
              - Import the paginator interface into the z-lib/http and
                export it from there

--- src/z-library/http.ts
+++ src/z-library/http.ts
@@ -1,4 +1,5 @@
 import { HttpResponse } from "./HTTP/http-response";
 import { httpErrors } from "./HTTP/http-errors";
+import { Paginator } from "./HTTP/paginator";
 
-export { HttpResponse, httpErrors }
+export { HttpResponse, httpErrors, Paginator }
