287bf48321d0e6ba7b5e5f2143f8e74fda146d71
GHOST-Aram <owinoojala@gmail.com>
2024-08-03 15:33:29 +0300

    test: Mock the new DataAccess method in events
          - Create a method to mock findExactMatch

--- src/apps/eventz/test/mocks/data-access.ts
+++ src/apps/eventz/test/mocks/data-access.ts
@@ -6,4 +6,12 @@ export class EventsDAL extends MockDataAccess<EventModel, Event>{
     constructor(model: EventModel, validData: Object){
         super(model, validData)
     }
+
+    public findExactMatch = async(eventData:Event) =>{
+        if(/Existing/i.test(eventData.title)){
+            return new this.model(eventData)
+        }
+
+        return null
+    }
 }
