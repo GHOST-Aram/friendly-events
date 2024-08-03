a0823160e725ac316665c0baaa44b248fa09ebe0
GHOST-Aram <owinoojala@gmail.com>
2024-08-03 16:40:00 +0300

    fix: Resolve the issue with duplicate creation of Event documents
        - Eliminate ageLimit from search parameters when trying to find an existing
          event document

--- src/apps/eventz/data-access/data-access.ts
+++ src/apps/eventz/data-access/data-access.ts
@@ -3,6 +3,15 @@ import { EventModel, Event } from "./model";
 
 export class EventsDataAccess extends GenericDataAccess<EventModel, Event>{
     public findExactMatch = async(eventdata:Event) =>{
-        return await this.model.findOne(eventdata)
+        return await this.model.findOne({
+            category: eventdata.category,
+            venue: eventdata.venue,
+            title: eventdata.title,
+            city: eventdata.city,
+            date: eventdata.date,
+            duration: eventdata.duration,
+            ticketPrice: eventdata.ticketPrice,
+            availableTickets: eventdata.availableTickets
+        })
     }
 }
