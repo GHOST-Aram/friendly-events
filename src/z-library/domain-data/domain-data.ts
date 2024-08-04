ff4a6ee4352ee893eba5d069a5fb60a68f609c1b
GHOST-Aram <owinoojala@gmail.com>
2024-08-04 19:13:05 +0300

    breaking: Add a new method to the DomainData interface
             - Add a method for creating search document with
               properties and values that should be unique for
               every document in the application DB
             - Method name - createUniqueSearchDocument

--- src/z-library/domain-data/domain-data.ts
+++ src/z-library/domain-data/domain-data.ts
@@ -1,6 +1,6 @@
 import { RequestData } from "../request"
 
 export interface DomainData{
-    aggregateInputDocument :(reqData: RequestData) => any
-    createUniqueSearchDocument: (inputData: any) => any
+    aggregateInputDocument :(reqData: RequestData) => {}
+    createUniqueSearchDocument: (inputData: any) => {}
 }
