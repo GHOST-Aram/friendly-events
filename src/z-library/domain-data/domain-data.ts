1158f25e35413e673c24d03044ea285bd39c8e09
GHOST-Aram <owinoojala@gmail.com>
2024-08-04 18:18:47 +0300

    breaking: Add a new method to DomainData interface
             - Add a method for creating unique search document that will
               be used to search for an exisiting document in the DB
               before creating a new one

--- src/z-library/domain-data/domain-data.ts
+++ src/z-library/domain-data/domain-data.ts
@@ -1,5 +1,6 @@
 import { RequestData } from "../request"
 
 export interface DomainData{
-    aggregateInputDocument :(reqData: RequestData) => Object
+    aggregateInputDocument :(reqData: RequestData) => any
+    createUniqueSearchDocument: (inputData: any) => any
 }
