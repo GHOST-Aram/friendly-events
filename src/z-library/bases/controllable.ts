54a1e8d57c22253b94371c1124d35c237f6f797b
GHOST-Aram <owinoojala@gmail.com>
2024-08-04 15:33:36 +0300

    breaking: revise the type definition of getMany method in the           Controllable interface
            - Accept a string array of searchable paths as a parameter
              and return an express route handler in the new definition

--- src/z-library/bases/controllable.ts
+++ src/z-library/bases/controllable.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express"
 export interface Controllable{
     addNew: (req: Request, res: Response, next: NextFunction) => Promise<void> 
     getOne: (req: Request, res: Response, next: NextFunction) => Promise<void>
-    getMany: (req: Request, res: Response, next: NextFunction) => Promise<void>
+    getMany: (searchablePaths: string[]) => (req: Request, res: Response, next: NextFunction) => Promise<void>
     updateOne: (req: Request, res: Response, next: NextFunction) => Promise<void>
     modifyOne: (req: Request, res: Response, next: NextFunction) => Promise<void>
     deleteOne: (req: Request, res: Response, next: NextFunction) => Promise<void>
