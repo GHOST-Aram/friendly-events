f42d078e34d15caab91ce2f7004b8967d629edf9
GHOST-Aram <owinoojala@gmail.com>
2024-08-04 15:42:10 +0300

    feat: Add a new method to the Accessible interface
         - Declare findBySearchDocument method

--- src/z-library/bases/accessible.ts
+++ src/z-library/bases/accessible.ts
@@ -4,10 +4,11 @@ import { Paginator } from "../http"
 export interface Accessible{
     createNew:(data: any) => Promise<HydratedDocument<any>>
     findByReferenceId:(refId: string) => Promise<HydratedDocument<any> | null>
+    findBySearchDocument: (searchDoc: any, paginator: Paginator) =>Promise<HydratedDocument<any>[]>
     findWithPagination: (paginator: Paginator) => Promise<HydratedDocument<any>[]>
     findByIdAndUpdate: (id: string, updateDoc: HydratedDocument<any>
         ) => Promise<HydratedDocument<any> | null>
-
+    
     findByIdAndDelete: (id: string) => Promise<HydratedDocument<any> | null>
     findByCreatorId: (creatorId: string, paginator:Paginator) => 
             Promise<HydratedDocument<any>[]>
