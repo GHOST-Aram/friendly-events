260f7b84e826520cc547e1fe96e45ddf744f41c9
GHOST-Aram <owinoojala@gmail.com>
2024-08-05 13:47:38 +0300

    chore: Remove unused imports from controller files
           - Remove unused imports from controller files of eventcategories,
             events, venues and venuetypes

--- src/apps/eventcategories/controller/controller.ts
+++ src/apps/eventcategories/controller/controller.ts
@@ -1,10 +1,5 @@
-import { Response, Request, NextFunction } from "../../../z-library/types";
 import { GenericController } from "../../../z-library/bases";
 import { DataAccess } from "../data-access/data-access";
-import { domainData } from "../domain/data";
-import { getDataFromRequest } from "../../../z-library/request";
-import { document } from "../../../z-library/document";
-import { userGroup } from "../../../utils/user-group/user-group";
 
 export class Controller extends GenericController<DataAccess>{
     constructor (dataAccess: DataAccess, microserviceName: string){
--- src/apps/eventz/controller/controller.ts
+++ src/apps/eventz/controller/controller.ts
@@ -1,10 +1,5 @@
-import { Response, Request, NextFunction } from "../../../z-library/types";
 import { GenericController } from "../../../z-library/bases";
 import { EventsDataAccess } from "../data-access/data-access";
-import { domainData } from "../domain/data";
-import { getDataFromRequest } from "../../../z-library/request";
-import { document } from "../../../z-library/document";
-import { userGroup } from "../../../utils/user-group/user-group";
 
 export class EventsController extends GenericController<EventsDataAccess>{
     constructor (dataAccess: EventsDataAccess, microserviceName: string){
