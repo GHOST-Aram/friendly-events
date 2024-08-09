7da7168a0b088bb7bfc48f381151fa321634f222
GHOST-Aram <owinoojala@gmail.com>
2024-08-09 21:32:54 +0300

    docs: Create openAPI specification file for venues and venuetypes

--- src/apps/open-api/app.ts
+++ src/apps/open-api/app.ts
@@ -11,15 +11,27 @@ const baseSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'openap
 const authSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'auth.json'), 'utf8'));
 const eventsSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'events.json'), 'utf8'));
 const usersSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'users.json'), 'utf8'));
-const eventcategoriesSpec = JSON.parse(fs.readFileSync(path.join(
-    __dirname, 'docs', 'eventcategories.json'), 'utf8'));
+const eventcategoriesSpec = JSON.parse(fs.readFileSync(path.join( __dirname, 'docs', 
+    'eventcategories.json'), 'utf8'));
+
+const venuesSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'venues.json'), 'utf8'));
+const venuesTypesSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 
+    'venuetypes.json'), 'utf8'));
     
 
 
 baseSpec.servers = updateServerInfo()
 
 
-const mergedSpecs = merge(baseSpec, authSpec, usersSpec, eventsSpec, eventcategoriesSpec)
+const mergedSpecs = merge(
+    baseSpec, 
+    authSpec, 
+    usersSpec, 
+    eventsSpec, 
+    eventcategoriesSpec, 
+    venuesSpec,
+    venuesTypesSpec
+)
 
 docsRouter.use('/', ...renderDocumentationUI(mergedSpecs))
 
