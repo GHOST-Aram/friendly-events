dea4393f0d2b6d6068dc7af6b95fc6736b73eb0b
GHOST-Aram <owinoojala@gmail.com>
2024-08-09 18:04:35 +0300

    Revert "feat: Intergrate swagger ui for auth documentation"
    This reverts commit 003f9e689bddd439e6c0403757c8dd0afed416fc.

--- src/apps/authenticator/app.ts
+++ src/apps/authenticator/app.ts
@@ -1,27 +1,12 @@
 import { AuthController } from "./controller/controller";
 import { DataAccess } from "./data-access/data-access";
 import { AuthRouter } from "./urls/urls";
-import path from 'path'
-import fs from 'fs'
-import { PORT } from "../../_environment";
-import swaggerUi from 'swagger-ui-express'
 
 const dataAccess  = new DataAccess()
 const controller = new AuthController(dataAccess)
 
-const authSpec = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'auth.json'), 'utf8'));
-authSpec.servers = [
-    {
-      "url": `http://localhost:${PORT}`,
-      "description": "Local server"
-    }
-  ];
-
-const authRouter = new AuthRouter(controller).registerRoutes()
 
 
-authRouter.use('/docs', swaggerUi.serve, swaggerUi.setup(authSpec) )
-
-export{ authRouter }
+export const authRouter = new AuthRouter(controller).registerRoutes()
 
 
