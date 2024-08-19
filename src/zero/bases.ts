8d1a96744e9af54eb7973ba218ca379673958150
GHOST-Aram <owinoojala@gmail.com>
2024-08-19 13:07:29 +0300

    refactor: Add Accessible and Controllable interfaces to zero/bases exports file

--- src/zero/bases.ts
+++ src/zero/bases.ts
@@ -1,4 +1,6 @@
 import { GenericController } from "./bases/generic-controller";
 import { GenericDataAccess } from "./bases/generic-data-access";
+import { Accessible } from "./bases/accessible";
+import { Controllable } from "./bases/controllable";
 
-export { GenericDataAccess, GenericController }
+export { GenericDataAccess, GenericController, Accessible, Controllable }
